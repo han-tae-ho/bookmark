0b9ee94b495639a4c17480478e91647814b913a3
han-tae-ho <gksdkfmacks@naver.com>
2021-02-18 19:31:15 +0900

    list view

--- bookmark/models.py
+++ bookmark/models.py
@@ -16,6 +16,10 @@ class Bookmark(models.Model):
     # 2. 제약 사항(몇글자까지, 한글만 ? )
     # 3. form의 종류 결정
     # 4. form 제약사항
+
+    def __str__(self):
+        return f"이름 : {self.site_name}, 주소 : {self.url}"
+
 # 모델을 만들었다. => 데이터베이스에 어떤 데이터들을 어떤 형태로 넣을지 결정!
 # 마이그레이션(migration) => 데이터베이스에 모델의 내용을 반영(테이블 수정)
 # makemigrations => 모델의 변경사항을 추적해서 기록
