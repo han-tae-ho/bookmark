c383ae8fdb9fa3f09a5dfeea86cf6b40ae557c78
han-tae-ho <gksdkfmacks@naver.com>
2021-02-18 22:31:00 +0900

    creat view

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BookmarkListView
+from .views import BookmarkListView, BookmarkCreateView
 
 urlpatterns = [
-    path("", BookmarkListView.as_view(), name='list')
+    path("", BookmarkListView.as_view(), name='list'),
+    path("add/", BookmarkCreateView.as_view(), name='add'),
     # 클래스형 뷰일 경우 as_veiw() 써야함
 ]
